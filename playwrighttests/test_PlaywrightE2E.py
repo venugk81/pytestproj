051a99de18dd21eec70cd1f4496e437f89de23b0
venug <venu.gk81@gmail.com>
2026-04-28 20:15:31 +0530

    pytest files updated

---
+++ playwrighttests/test_PlaywrightE2E.py
@@ -0,0 +1,34 @@
+import time
+
+import playwright
+import pytest
+from playwright.sync_api import Playwright
+
+from pytest_param.FileUtils import get_excel_data
+
+
+# from playwright.sync_api import sync_playwright
+@pytest.mark.parametrize("map_data", get_excel_data("employee_data", "E001", "Employee"))
+def test_fixture(playwright: Playwright, map_data):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://register.rediff.com/register/register.php?FormName=user_details")
+    # page.get_by_title("Search").fill("testing")
+    # page.get_by_title("Search").press("Enter")
+    page.get_by_placeholder("Enter your full name").fill("Venu Gopal")
+    page.get_by_placeholder("Enter Rediffmail ID", exact=False).fill("gopivenu9kjasdfkasdf")
+    page.get_by_placeholder("Enter password", exact=False).fill("hagsdfkjahsdgf$")
+    page.get_by_placeholder("Retype password", exact=False).fill("hagsdfkjahsdgf$")
+    page.locator('select[name^="DOB_Day"]').select_option("07")
+    page.locator('select[name^="DOB_Month"]').select_option("JUN")
+    page.locator('select[name^="DOB_Year"]').select_option("1986")
+    page.locator('[name^="gender"][value="f"]').check()
+    page.locator("[id='country']").select_option("Australia")
+    page.get_by_placeholder("Enter recovery email").fill("kjadsf@gmail.com")
+    # page.locator('[name^="chk"]').check()
+    page.locator('[id="mobno"]').fill("7773884")
+    time.sleep(2)
+    page.close()
+    browser.close()
+
